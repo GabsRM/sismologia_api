c4aa872f7cab872faeec697f86a06a6d2fb9defe
Engel Reyes <engelreyeser@gmail.com>
2024-04-12 11:58:56 -0600

    refactor: Configurando cors para permitir solicitudes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,10 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins 'localhost:3001'  # Cambia esto según el origen desde el que estás haciendo la solicitud
+      resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options]
+    end
+  end
 end
